b212d1cf1aa6e33442c7001ad0c99d6c53f46f89
zwj <841752349@qq.com>
2024-04-12 16:50:11 +0800

    1、新增card编写

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,12 +1,19 @@
 // list
+import Image from 'next/image'
+import { useState } from 'react'
 
 const Card = () => {
   return <div className="rounded-lg p-2 border border-gray-200 overflow-hidden w-full h-60 duration-150 flex flex-col">
       <div className="rounded-lg w-full h-3/4 bg-gray-200 group/bg relative">
-        <div className="invisible group-hover/bg:visible size-full flex justify-center items-center gap-6">
-          <div>sd</div>
-          <div>sd</div>
-          <div>sd</div>
+      <div className="rounded-lg invisible group-hover/bg:visible size-full absolute shadow blur-sm"></div>
+        <div className="invisible group-hover/bg:visible size-full flex justify-center items-center gap-6 absolute z-1">
+          <Image
+              src="/static/icons/heart-pulse.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
         </div>
       </div>
       <div className="p-2 pt-1 pb-0 grow flex flex-col">
@@ -20,6 +27,16 @@ const Card = () => {
 }
 
 export default function Page() {
+  const [state, setState] = useState({
+    mode: 0,
+  })
+
+  const switchRender = () => {
+    switch(state.mode) {
+      case 0:
+        break;
+    }
+  }
   return (
     <>
     <header className="bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0 lg:border-0">
@@ -29,6 +46,22 @@ export default function Page() {
     </header>
     <main className="p-24 mx-auto">
       <div className="size-full overflow-auto">
+        <div className="p-4 flex flex-row-reverse w-full gap-6">
+          <Image
+              src="/static/icons/list.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+            <Image
+              src="/static/icons/grip.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer"
+            />
+        </div>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {
             new Array(20).fill(0).map((e, index) => {
